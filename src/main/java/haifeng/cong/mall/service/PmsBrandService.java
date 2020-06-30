4f5be4c481d6ebabb6666b50d1a297a5a1d85cd6
conghaifeng <conghaifeng@ruijie.com.cn>
2020-06-30 10:36:43 +0800

    Initial commit

---
+++ src/main/java/haifeng/cong/mall/service/PmsBrandService.java
@@ -0,0 +1,12 @@
+package haifeng.cong.mall.service;
+
+/**
+ * Description:
+ * Copyright: Copyright (c) 2020
+ * Company: Ruijie Co., Ltd.
+ * Create Time: 2020/6/29 16:46
+ *
+ * @author conghaifeng
+ */
+public interface PmsBrandService {
+}
